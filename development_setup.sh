2f373f6ea5844c9d4d5461808f516dcc0b2a0ac3
averagengineer <me@danielshare.co.uk>
2020-06-22 12:54:51 +0100

    Removed unnecessary git clone from setup script

--- development_setup.sh
+++ development_setup.sh
@@ -1,7 +1,2 @@
 # Create the credentials file for the user to populate
 touch AUTH_TOKEN
-
-# Clone dependencies in to the include directory
-git clone https://github.com/Tencent/rapidjson.git
-mv rapidjson/include/rapidjson include/
-rm -rf rapidjson
