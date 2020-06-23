aae2f073e48313d4086c3b7a8b9e01418d653648
averagengineer <me@danielshare.co.uk>
2020-06-23 18:43:13 +0100

    Created function to print to console

--- Debug.cpp
+++ Debug.cpp
@@ -1,8 +1,18 @@
+#include <iostream>
 #include "Debug.h"
 
 Debug::Debug(bool debugMode) {
     // Set the given debug mode for the class
     this->debug = debugMode;
+}
+
+void Debug::Print(string debugString) {
+    // If the program is running in debug mode
+    if(this->debug)
+    {
+        // Print the given string
+        cout << debugString << endl;
+    }
 }
 
 
--- Debug.h
+++ Debug.h
@@ -1,15 +1,20 @@
 #ifndef BAKUP_AGENT_DEBUG_H
 #define BAKUP_AGENT_DEBUG_H
 
+#include <string>
+
+using namespace std;
 
 class Debug {
     // Is the program in debug mode?
     bool debug;
 
 public:
-    Debug(bool debugMode);
+    // Constructor that takes the debug mode of the program
+    explicit Debug(bool debugMode);
 
-
+    // Print a string to console
+    void Print(string debugString);
 };
 
 
