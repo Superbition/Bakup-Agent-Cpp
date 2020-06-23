c10b2267e5964cf93279fa14398c9184971a60bd
averagengineer <me@danielshare.co.uk>
2020-06-23 18:34:05 +0100

    Add Debug class for printing when not daemonised

---
+++ Debug.cpp
@@ -0,0 +1,8 @@
+#include "Debug.h"
+
+Debug::Debug(bool debugMode) {
+    // Set the given debug mode for the class
+    this->debug = debugMode;
+}
+
+
---
+++ Debug.h
@@ -0,0 +1,16 @@
+#ifndef BAKUP_AGENT_DEBUG_H
+#define BAKUP_AGENT_DEBUG_H
+
+
+class Debug {
+    // Is the program in debug mode?
+    bool debug;
+
+public:
+    Debug(bool debugMode);
+
+
+};
+
+
+#endif //BAKUP_AGENT_DEBUG_H
