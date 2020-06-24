c5233066bfcf2a4da44e02ea67941d9f5748e9ce
averagengineer <me@danielshare.co.uk>
2020-06-24 11:42:07 +0100

    Create command class

---
+++ Command.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dan on 24/06/2020.
+//
+
+#include "Command.h"
---
+++ Command.h
@@ -0,0 +1,14 @@
+//
+// Created by dan on 24/06/2020.
+//
+
+#ifndef BAKUP_AGENT_COMMAND_H
+#define BAKUP_AGENT_COMMAND_H
+
+
+class Command {
+
+};
+
+
+#endif //BAKUP_AGENT_COMMAND_H
