f36b4a48382d1896728c955cbe0004ae38d81fea
averagengineer <me@danielshare.co.uk>
2020-08-31 22:38:52 +0100

    Added function signatures and inline declarations

--- Response.cpp
+++ Response.cpp
@@ -1,5 +1,14 @@
-//
-// Created by dan on 31/08/2020.
-//
-
 #include "Response.h"
+
+Response::Response(string url, string authToken) {
+
+}
+
+int Response::apiPostData(cpr::Header &headers, string &postData, string &postResponse) {
+    return 0;
+}
+
+int
+Response::postJobConfirmation(const string &authorisationToken, string &postData, string &postResponse) {
+    return 0;
+}
