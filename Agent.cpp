7b76d380712d14f245ffabf81bac3bb3c6801bd2
averagengineer <me@danielshare.co.uk>
2020-10-02 13:30:19 +0100

    Added function to get user ID

--- Agent.cpp
+++ Agent.cpp
@@ -34,6 +34,10 @@ string Agent::getAuthToken()
     return this->authToken;
 }
 
+string Agent::getUserID() {
+    return this->userID;
+}
+
 string Agent::getBakupJobConfirmationURL()
 {
     return this->host + this->baseUrl + this->apiVersionBaseUrl + this->apiVersion + this->bakupJobConfirmationUrl;
--- Agent.h
+++ Agent.h
@@ -1,6 +1,8 @@
 #ifndef BAKUP_AGENT_AGENT_H
 #define BAKUP_AGENT_AGENT_H
 #include <string>
+#include <sys/types.h>
+#include <unistd.h>
 
 using namespace std;
 
@@ -8,16 +10,22 @@ class Agent
 {
     private:
         // Folder for configuration files
-        const string configDirectory = "/etc/bakupagent";
+        const string configDirectory = "/etc/opt/bakupagent";
 
         // Working directory for storing temp data
         const string workingDirectory = "/tmp";
 
-        // Location of the config file
+        // Location of the authentication token
         const string authorisationLocation = configDirectory + "/AUTH_TOKEN";
 
-        // Get the config file contents
+        // Get the authentication token
         const string authToken = this->readFile(authorisationLocation);
+
+        // Location of the user ID to run the program as
+        const string userIDLocation = configDirectory + "/USER_ID";
+
+        // Get the user ID
+        const string userID = this->readFile(userIDLocation);
 
         // Host URL
         const string host = "localhost";
@@ -46,6 +54,9 @@ class Agent
 
         // Get the auth token
         string getAuthToken();
+
+        // Get the user ID
+        string getUserID();
 
         // Generate a bakup request url
         string getBakupRequestURL();
