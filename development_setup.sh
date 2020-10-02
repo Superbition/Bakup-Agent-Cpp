0253e010a841c6b9cee5e43199903015eeccc4b8
averagengineer <me@danielshare.co.uk>
2020-10-02 16:31:58 +0100

    Now requires root to run to make automation easier

--- development_setup.sh
+++ development_setup.sh
@@ -1,5 +1,5 @@
 # Check if the authentication token is supplied
-if [[ $# -eq 0 ]]
+if [ $# -eq 0 ]
   then
     echo "An authentication token must be supplied"
     exit 1
@@ -7,20 +7,21 @@ fi
 
 # Create the directories needed for storing files and the binary
 echo "Creating directories..."
-sudo mkdir -p /opt/bakupagent
-sudo mkdir -p /etc/opt/bakupagent
+mkdir -p /opt/bakupagent
+mkdir -p /etc/opt/bakupagent
 
 # Create the credentials file for the user to populate
 echo "Populating the authentication token..."
 AUTH_TOKEN=$1
-sudo touch /etc/opt/bakupagent/AUTH_TOKEN
-echo $AUTH_TOKEN | sudo tee /etc/opt/bakupagent/AUTH_TOKEN > /dev/null
+touch /etc/opt/bakupagent/AUTH_TOKEN
+echo $AUTH_TOKEN | tee /etc/opt/bakupagent/AUTH_TOKEN > /dev/null
 
 # Get the user's ID
 echo "Obtaining user ID..."
-USER_ID=$(id -u)
-sudo touch /etc/opt/bakupagent/USER_ID
-echo $USER_ID | sudo tee /etc/opt/bakupagent/USER_ID > /dev/null
+USER_NAME=$(logname)
+USER_ID=$(id -u $USER_NAME)
+touch /etc/opt/bakupagent/USER_ID
+echo $USER_ID | tee /etc/opt/bakupagent/USER_ID > /dev/null
 
 # Create the service file to manage the service
 echo "Making service file for systemd..."
@@ -34,6 +35,6 @@ Type=simple
 ExecStart=/opt/bakupagent/bakupagent
 
 [Install]
-WantedBy=multi-user.target" | sudo tee /etc/systemd/system/bakupagent.service > /dev/null
+WantedBy=multi-user.target" | tee /etc/systemd/system/bakupagent.service > /dev/null
 
 echo "DONE."
