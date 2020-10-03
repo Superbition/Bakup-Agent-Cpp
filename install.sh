37847f43a9fd21ca35fbab066260febea5dfaf1c
averagengineer <me@danielshare.co.uk>
2020-10-03 12:51:29 +0100

    Check if the user is root

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+# Check if the user is root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit 1
+fi
+
 # Check if the authentication token is supplied
 if [ $# -eq 0 ]
   then
