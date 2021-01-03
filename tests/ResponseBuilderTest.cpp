092ee5adbf4c8ac02fd6f4acc1c63e3a0cb1b03d
averagengineer <me@danielshare.co.uk>
2021-01-03 14:01:14 +0000

    Add test cases

---
+++ tests/ResponseBuilderTest.cpp
@@ -0,0 +1,67 @@
+#include <gtest/gtest.h>
+
+#include <rapidjson/document.h>
+#include <rapidjson/writer.h>
+#include <rapidjson/stringbuffer.h>
+#include <ResponseBuilder.h>
+#include <vector>
+
+class ResponseBuilderTest : public ::testing::Test
+{
+    protected:
+        ResponseBuilderTest()
+        {
+
+        }
+
+        ~ResponseBuilderTest()
+        {
+
+        }
+
+    public:
+
+};
+
+TEST_F(ResponseBuilderTest, BuildJson)
+{
+    ResponseBuilder responseBuilder;
+    ASSERT_EQ(responseBuilder.build(), "{}");
+}
+
+TEST_F(ResponseBuilderTest, AddErrorCode)
+{
+    ResponseBuilder responseBuilder;
+    responseBuilder.addErrorCode(0);
+    ASSERT_EQ(responseBuilder.build(), "{\"error_code\":0}");
+}
+
+TEST_F(ResponseBuilderTest, AddErrorMessage)
+{
+    ResponseBuilder responseBuilder;
+    responseBuilder.addErrorMessage("test");
+    ASSERT_EQ(responseBuilder.build(), "{\"error_message\":\"test\"}");
+}
+
+TEST_F(ResponseBuilderTest, AddSendAttempt)
+{
+    ResponseBuilder responseBuilder;
+    responseBuilder.addSendAttempt(0);
+    ASSERT_EQ(responseBuilder.build(), "{\"send_attempt\":0}");
+}
+
+TEST_F(ResponseBuilderTest, AddCommandOutputs)
+{
+    ResponseBuilder responseBuilder;
+    vector<commandOutput> commandsOutput;
+    commandOutput temp;
+    temp.command = "test_command";
+    temp.statusCode = 0;
+    temp.result = "test_result";
+    commandsOutput.push_back(temp);
+    commandsOutput.push_back(temp);
+    responseBuilder.addCommandOutputs(commandsOutput);
+
+    // Build and test string
+    ASSERT_EQ(responseBuilder.build(), "{\"command_output\":[{\"command\":\"test_command\",\"status_code\":0,\"result\":\"test_result\"},{\"command\":\"test_command\",\"status_code\":0,\"result\":\"test_result\"}]}");
+}
