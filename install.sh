7589ad97849867d48c9735002b7fbb325f52d110
averagengineer <me@danielshare.co.uk>
2021-01-04 13:10:34 +0000

    Made auth files accessible to user

--- install.sh
+++ install.sh
@@ -27,24 +27,26 @@ echo "Creating directories..."
 mkdir -p /opt/bakupagent
 mkdir -p /etc/opt/bakupagent
 
+# Get the user's ID
+echo "Obtaining user ID..."
+USER_NAME=$(logname)
+USER_ID=$(id -u "$USER_NAME")
+touch /etc/opt/bakupagent/USER_ID
+echo "$USER_ID" | tee /etc/opt/bakupagent/USER_ID > /dev/null
+
 # Create the credentials file for the user to populate
 echo "Populating the client ID..."
 CLIENT_ID=$1
 touch /etc/opt/bakupagent/CLIENT_ID
 echo "$CLIENT_ID" | tee /etc/opt/bakupagent/CLIENT_ID > /dev/null
+chown "$USER_NAME" /etc/opt/bakupagent/CLIENT_ID
 
 # Create the credentials file for the user to populate
 echo "Populating the authentication token..."
 AUTH_TOKEN=$2
 touch /etc/opt/bakupagent/AUTH_TOKEN
 echo "$AUTH_TOKEN" | tee /etc/opt/bakupagent/AUTH_TOKEN > /dev/null
-
-# Get the user's ID
-echo "Obtaining user ID..."
-USER_NAME=$(logname)
-USER_ID=$(id -u "$USER_NAME")
-touch /etc/opt/bakupagent/USER_ID
-echo $USER_ID | tee /etc/opt/bakupagent/USER_ID > /dev/null
+chown "$USER_NAME" /etc/opt/bakupagent/AUTH_TOKEN
 
 # Create the service file to manage the service
 echo "Making service file for systemd..."
