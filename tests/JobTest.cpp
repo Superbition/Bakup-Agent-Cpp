5bacb1bf1a334882dd003dd6253d40893807c027
averagengineer <me@danielshare.co.uk>
2021-01-24 20:36:26 +0000

    Removed unused check shell tests

--- tests/JobTest.cpp
+++ tests/JobTest.cpp
@@ -69,38 +69,3 @@ TEST_F(JobTest, HandleErrors)
     string httpError = "HTTP ERROR";
     ASSERT_TRUE(jobObj.handleError(httpError, error));
 }
-
-TEST_F(JobTest, CheckShellReady)
-{
-    // Create the command struct
-    command_t job;
-    job.id = "1";
-    job.targetExecutionTime = time(NULL);
-    job.commands.emplace_back("ls");
-
-    // Construct the debug library for output
-    Debug debug(true, agent.getAgentVersion());
-
-    Command command(debug);
-    command.setupEnvironment();
-    Job jobObj(debug, job, agent.getBaseURL(), agent.getClientId(), agent.getApiToken(), false);
-    ASSERT_TRUE(jobObj.checkShellReady(command, 1, 3));
-}
-
-TEST_F(JobTest, CheckShellFails)
-{
-    // Create the command struct
-    command_t job;
-    job.id = "1";
-    job.targetExecutionTime = time(NULL);
-    job.commands.emplace_back("ls");
-
-    // Construct the debug library for output
-    Debug debug(true, agent.getAgentVersion());
-
-    Job jobObj(debug, job, agent.getBaseURL(), agent.getClientId(), agent.getApiToken(), false);
-
-    string invalidShell = "/bin/touch";
-    Command command(debug, invalidShell);
-    ASSERT_FALSE(command.setupEnvironment("notAValidCommand"));
-}
