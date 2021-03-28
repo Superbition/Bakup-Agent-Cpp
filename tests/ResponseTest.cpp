701e868a3358973f1245cd87f805d2e77a6681ce
averagengineer <me@danielshare.co.uk>
2021-03-28 10:34:05 +0100

    Update usage of Job, Response, and Request in all tests

--- tests/ResponseTest.cpp
+++ tests/ResponseTest.cpp
@@ -17,7 +17,7 @@ class ResponseTest : public ::testing::Test
 TEST_F(ResponseTest, PostJobConfirmation)
 {
     string jobStatus = "[{\"test\": true}]";
-    Response response(this->agent.getBaseURL(), this->agent.getClientId(), this->agent.getApiToken());
+    Response response(this->agent.getBaseURL(), this->agent.getClientId(), this->agent.getApiToken(), this->agent.getAgentVersion());
     int statusCode = response.postJobConfirmation(jobStatus);
     ASSERT_GE(statusCode, 200);
     ASSERT_LT(statusCode, 500);
