dd50c4b8695c0e4bb326eb10a4ab2183ce06142d
averagengineer <me@danielshare.co.uk>
2021-07-31 10:50:07 +0100

    Update Agent version to v6.0.0

--- src/Agent.h
+++ src/Agent.h
@@ -65,7 +65,7 @@ class Agent
         const string apiVersion = "1";
 
         // Version of the agent
-        const string agentVersion = "v5.0.6";
+        const string agentVersion = "v6.0.0";
 
         // Program loop wait time in seconds
         const int pollTime = 60;
